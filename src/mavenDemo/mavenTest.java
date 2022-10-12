0420f75bea8541756da574d7790f37d806e316fe
verma-raj <jobhunt.verma@gmail.com>
2022-10-12 16:29:51 +0530

    Initial java code for mvn build

---
+++ src/mavenDemo/mavenTest.java
@@ -0,0 +1,11 @@
+package mavenDemo;
+
+public class mavenTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Hello We are demonstrating Maven Build");
+
+	}
+
+}
